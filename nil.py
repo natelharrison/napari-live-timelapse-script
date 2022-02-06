1a8cb5e35d6aa081facbdfd897c203ae1618a4dc
Nate <78279182+Myrkgod@users.noreply.github.com>
2022-02-05 20:55:12 -0800

    Glob sorted files and no nested functions
    *Replaced os with glob
    *getFiles() now returns a list of filed in numerical order
    *REMOVED NESTED FUNCTIONS!!!!!!!!!!!!!!!!!!!!!!!!

--- nil.py
+++ nil.py
@@ -1,41 +1,45 @@
 from napari.qt.threading import thread_worker
-import napari, tifffile, os, time
+from glob import glob
+import napari, tifffile, time
+
+directory = 'MIPs'
+channel = 'CamA_ch0,CamB_ch0,CamC_ch0'
+
+#Returns new files in a folder
+newFiles = []
+oldFiles = []
+def getFiles():
+    newFiles.clear()
+    for file in glob(directory+'/*tif'):
+        if file not in oldFiles:
+            newFiles.append(file)
+            oldFiles.append(file)
+    newFiles.sort(key=lambda fname: int(fname.split('_')[3]))
+    return newFiles
+
+#Returns list of all new files in tiff format
+def tiffImage(): 
+    newList = getFiles()
+    im = []
+    for tif in newList:
+        print(tifffile.imread(tif))
+        im.append(tifffile.imread(tif))
+    return im
 
 @thread_worker
 def runCycle():
-
-    #Returns new files in a folder
-    newFiles = []
-    oldFiles = []
-    def getFiles():
-        newFiles.clear
-        for file in os.listdir('MIPs'):
-            if file not in oldFiles:
-                newFiles.append(file)
-                oldFiles.append(file)
-        return newFiles  
-    
-    #Returns list of all new files in tiff format
-    def tiffImage(): 
-        tifList = []
-        im = []
-        for file in getFiles():
-            tifList.append('MIPs/'+file)
-
-        for tif in tifList:
-            im.append(tifffile.imread(tif))
-        return im
-
-    #Yields files for connecter and sleeps for duration 
     while True:
         for file in tiffImage():
-            yield file
+            yield file 
         time.sleep(10)
 
 
 viewer = napari.Viewer()
 worker = runCycle()
 worker.yielded.connect(viewer.add_image)
+
 worker.start()
 
 napari.run()
+
+
