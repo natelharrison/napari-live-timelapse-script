1d762da8500336ed24316677ceb787f8914e8e6a
Nate <78279182+Myrkgod@users.noreply.github.com>
2022-02-07 10:39:31 -0800

    Removed tiff function

--- nil.py
+++ nil.py
@@ -21,7 +21,6 @@ def getFiles():
     newFiles.sort(key=lambda fname: int(fname.split('_')[3]))
     return newFiles
 
-
 def addImage(image):
     for channel in channelList:
         if channel in image:
@@ -29,23 +28,12 @@ def addImage(image):
             im = tifffile.imread(image)
             viewer.add_image(im, name=imName)
 
-
-#Returns list of all new files in tiff format
-def tiffImage(): 
-    newList = getFiles()
-    im = []
-    for tif in newList:
-        print(tifffile.imread(tif))
-        im.append(tifffile.imread(tif))
-    return im
-
 @thread_worker(connect={'yielded': addImage})
 def runCycle():
     while True:
         for file in getFiles():
             yield file
         time.sleep(10)
-
 
 viewer = napari.Viewer()
 
