3531d65f98383ed0d00bc75063cf08eccf012550
Myrkgod <natelharrison@berkeley.edu>
2022-02-09 10:49:03 -0800

    Load images as stacks by channel

--- nil.py
+++ nil.py
@@ -1,6 +1,8 @@
 from napari.qt.threading import thread_worker
+from qtpy.QtWidgets import QPushButton
 from glob import glob
 import napari, tifffile, time
+import numpy as np
 
 directory = 'MIPs'
 channels = 'CamA_ch0,CamB_ch0'
@@ -8,8 +10,6 @@ colorMaps = 'blue,green'
 
 colorList = colorMaps.split(',')
 channelList = channels.split(',')
-
-#Returns new files in a folder
 
 oldFiles = []
 def getFiles():
@@ -21,23 +21,43 @@ def getFiles():
     newFiles.sort(key=lambda fname: int(fname.split('_')[3]))
     return newFiles
 
-def addImage(image):
-    for channel in channelList:
-        if channel in image:
-            imName = lambda fname: int(fname.split('_')[2:3])
-            im = tifffile.imread(image)
-            viewer.add_image(im, name=imName)
+imArrayList = []
+def addImages(fileList):   
+    if not fileList:
+        return  
+    channel = fileList[1].split('_')[4] + '_' + fileList[1].split('_')[5] 
+    colormap = colorList[channelList.index(channel)]
+    tifList = []
+    for image in fileList:       
+        tifList.append(tifffile.imread(image))
+    print(channel)
+    timelapse = np.asarray(tifList)
+    viewer.add_image(timelapse, name=channel, colormap=colormap)
 
-@thread_worker(connect={'yielded': addImage})
+@thread_worker(connect={'yielded': addImages})
 def runCycle():
     while True:
-        for file in getFiles():
-            yield file
+        newFiles = getFiles()
+        for channel in channelList:
+            fileList = []
+            for file in newFiles:
+                if channel in file:
+                    fileList.append(file)
+            yield fileList
         time.sleep(10)
 
+
 viewer = napari.Viewer()
+
+# add a button to the viewew that, when clicked, stops the worker
+button = QPushButton("STOP!")
+button.clicked.connect(runCycle().quit)
+runCycle().finished.connect(button.clicked.disconnect)
+viewer.window.add_dock_widget(button)
 
 runCycle()
 
 napari.run()
+
+
 
