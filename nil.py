550661fca3c94f9eb067b251cde60cf704e644a5
Myrkgod <natelharrison@berkeley.edu>
2022-02-17 23:12:57 -0800

    Fixed default value issue with contrast_limits

--- nil.py
+++ nil.py
@@ -4,8 +4,8 @@ import napari, tifffile, time, argparse, random
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('channels', type=str, default='CamA_ch0,CamB_ch0')
 parser.add_argument('directory', type=str, default='MIPs')
+parser.add_argument('channels', type=str, default='CamA_ch0,CamB_ch0')
 parser.add_argument('--opacity', type=float, nargs='+', default=[1.0])          
 parser.add_argument('--contrast_limits', type=float, nargs='+', default=[None])   
 parser.add_argument('--gamma', type=float, nargs='+', default=[1.0])             
@@ -18,6 +18,8 @@ parser.add_argument('--fetch_interval', type=int, default=10)
 parser.add_argument('--layer_buffer', type=int, default=2)        
 args = parser.parse_args()                                    
 
+
+viewer = napari.Viewer()
 channel_list = args.channels.split(',')
 def add_images(file_list):   
     if not file_list:
@@ -30,18 +32,22 @@ def add_images(file_list):
     for image in file_list:       
         tif_list.append(tifffile.imread(image, name=channel))
     timelapse = np.asarray(tif_list)
-    
+
     if len(viewer.layers) >= len(channel_list):
         layer = viewer.layers[index]   
         layer.data = np.concatenate((layer.data, timelapse), axis=0)
     else:
+        contrast_limits = args.contrast_limits[2*index % len(args.contrast_limits):
+                                               2*index % len(args.contrast_limits)+2]
+        if contrast_limits[0] is None:
+            contrast_limits = None
+    
+        print(contrast_limits)
         viewer.add_image(
             timelapse, name=channel, opacity=args.opacity[index % len(args.opacity)], 
-            contrast_limits=args.contrast_limits[index % len(args.contrast_limits)], 
-            gamma=args.gamma[index % len(args.gamma)], colormap=args.colormaps.split(',')[index], 
-            blending=args.blending, interpolation=args.interpolation)
+            contrast_limits=contrast_limits, gamma=args.gamma[index % len(args.gamma)], 
+            colormap=args.colormaps.split(',')[index], blending=args.blending, interpolation=args.interpolation)
 
-viewer = napari.Viewer()
 @thread_worker(connect={'yielded': add_images})
 def fetch_files():
     old_files = []
