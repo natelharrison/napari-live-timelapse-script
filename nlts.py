787fa2cbdf3840b471a827739d45b0f10d658318
Myrkgod <natelharrison@berkeley.edu>
2022-02-21 23:29:38 -0800

    Added --help to args

--- nlts.py
+++ nlts.py
@@ -6,14 +6,22 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', type=str)
 parser.add_argument('channels', type=str)
-parser.add_argument('--opacity', type=float, nargs='+', default=[1.0])          
-parser.add_argument('--contrast_limits', type=float, nargs='+', default=[None])   
-parser.add_argument('--gamma', type=float, nargs='+', default=[1.0])             
-parser.add_argument('--colormaps', type=str, default='blue,green,red,magenta,yellow,orange,cyan')       
-parser.add_argument('--blending', type=str, default='additive')       
-parser.add_argument('--interpolation', type=str, default='nearest')                                        
-parser.add_argument('--fetch_interval', type=int, default=10)                                           
-parser.add_argument('--layer_buffer', type=int, default=2)        
+parser.add_argument('--opacity', type=float, nargs='+', default=[1.0],
+    help='Space seperated floats (0.0-1.0) for each channel. --opacity 1.0 1.0')          
+parser.add_argument('--contrast_limits', type=float, nargs='+', default=[None],
+    help='Two space seperated floats for each channel. --contrast_limits 0.0 100.0 0.0 100.0')   
+parser.add_argument('--gamma', type=float, nargs='+', default=[1.0], 
+    help='Space seperated floats (0.0-2.0) for each channel. --gamma 1.0 1.0')             
+parser.add_argument('--colormaps', type=str, default='blue,green,red,magenta,yellow,orange,cyan',
+    help='Comma seperated string with colormap for each channel. --colormaps blue,green')    
+parser.add_argument('--blending', type=str, default='additive', 
+    help='Single string for blending method. --blending additive')       
+parser.add_argument('--interpolation', type=str, default='nearest'
+    help='Single string for interpolation method. --interpolation nearest')                                        
+parser.add_argument('--fetch_interval', type=int, default=10, 
+    help='Duration to wait between checking folder for new files (seconds)')
+parser.add_argument('--layer_buffer', type=int, default=2, 
+    help='Amount of layers to leave out when processing new files')       
 args = parser.parse_args()                                    
 
 
